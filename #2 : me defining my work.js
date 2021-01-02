b3339862fc28342f489d98f3475c225c246b0e75
javascriptuncle <javascriptuncle@gmail.com>
2021-01-02 23:13:55 +0530

    #2 : me defining my work 🔥

---
+++ #2 : me defining my work.js
@@ -0,0 +1,20 @@
+// me defining my work
+life.day = 1;
+life.uncertainity = {
+	unexpectedDay: '!?'
+}
+
+for (; ;) {
+	console.log('Post daily 🔥');
+
+	if (life.day == life.uncertainity.unexpectedDay) {
+		console.log('No Post that day');
+	}
+	
+	// like, share and follow
+	if ('❤️ my work') {
+		console.log('🙏 Stay ❤️ tuned 🙏');
+	}
+
+	life.day += 1;
+}
