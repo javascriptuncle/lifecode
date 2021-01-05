026ba3a60bc1e4ff9ce11624f6536e1eb47c5594
javascriptuncle <javascriptuncle@gmail.com>
2021-01-05 23:59:28 +0530

    #5 : me when nothing to post

---
+++ #5 : me when nothing to post.js
@@ -0,0 +1,21 @@
+// me when nothing to post
+
+// I Breathe
+async myLife['🧬'].body.breathes();
+
+// I Think
+async myLife['🧬'].mind.thinks('lot')
+
+// Connect the dots
+async function connectDots(){
+	let todayPOST = await myLife.closeEyes().connect('dots')
+	return todayPOST;
+}
+
+// I Write & Post
+async myLife.write(connectDots()).post()
+
+// like, share and follow
+if ('❤️ my work') {
+	console.log('🙏 Stay ❤️ tuned 🙏');
+}
