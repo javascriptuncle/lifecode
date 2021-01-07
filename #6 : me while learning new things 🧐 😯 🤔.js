4feae969932e8b5103b9d0d1ce443cea74200eef
javascriptuncle <javascriptuncle@gmail.com>
2021-01-07 23:59:37 +0530

    #6 : me while learning new things 🧐 😯 🤔

---
+++ #6 : me while learning new things 🧐 😯 🤔.js
@@ -0,0 +1,21 @@
+// me while learning new things 🧐 😯 🤔
+
+myLife.day
+	.startsWith("💡 Learn New Library!").search("Resources in Google")
+	.learnWithCuriosity("🧐 😯 🤔 📚 💻")
+	.recieveNotification("Some Magical 🪄 Video from Youtube")
+	.playsInLoop("📽️").parallelly("Social Media Atrraction📱")
+	.timepasses("⏳ away")
+	.getsWildReminder("🙄 I was into something 😬")
+	.dayEndsWith((learnedThings)=>{
+
+			// What I learned
+			console.log("How to, setup environement! + get Distracted");
+
+			console.log(learnedThings == "lot");  // false
+			
+			// like, share and follow
+			if ('❤👌 my work') {
+				console.log('🙏 Stay ❤️ tuned 🙏');
+			}
+	})
