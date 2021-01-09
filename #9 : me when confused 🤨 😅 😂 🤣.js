2d9c6d96f0aa16b979ab9c0f3e742ac7f126cae9
javascriptuncle <javascriptuncle@gmail.com>
2021-01-09 20:38:42 +0530

    #9 : me when confused 🤨 😅 😂 🤣

---
+++ #9 : me when confused 🤨 😅 😂 🤣.js
@@ -0,0 +1,19 @@
+// me when confused 🤨 😅 😂 🤣
+
+// disable BTN using (&&AND ||OR !NOT) Logic
+myLife.onWork().confusedState((task)=>{
+
+	// options
+	(value1 && value2) OR (value1 OR value2) OR (!value1 && !value2) OR (!value1 OR !value2) OR (value1 && !value2) OR (!value1 && value2) OR (value1 OR !value2) OR (!value1 OR value2) OR  !(value1 && value2) OR (true)
+	
+	// I ask my self
+	// 🤨 😅 😂 🤣 "which will work!";
+	
+	// Using which one you get confused?
+	console.log('🙏 Let me know in Comments 🙏');
+
+	// like, share and follow
+	if ('❤👌 my work') {
+		console.log('🙏 Stay ❤️ tuned 🙏');
+	}
+})
