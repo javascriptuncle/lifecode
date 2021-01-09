a938058f9f3e5b01da8c2d37928d283680132b21
javascriptuncle <javascriptuncle@gmail.com>
2021-01-09 20:38:00 +0530

    #7 : me while coding 🤓 📚 💻

---
+++ #7 : me while coding 🤓 📚 💻.js
@@ -0,0 +1,21 @@
+// me while coding 🤓 📚 💻
+
+var activityList = [
+	"MDN", // read something get context
+	"Medium", // any article on it
+	"Github", // any examples on it
+	"Stackoverflow", // any related issue
+	...readGoogleLinks('Related to Code!'),
+	"Code", // actual coding
+];
+
+mylife.code(activityList.join("=>")).result((output)=>{
+	console.log("It works 💡", output); // It works 💡 ** SOME INTERESTING STUFF **
+})
+.lesson("more effort" != "perfect output").timeConsumed("⌛ full day")
+.but("Get 💎 cristal clear what to do 😎!")
+
+// like, share and follow
+if ('❤👌 my work') {
+	console.log('🙏 Stay ❤️ tuned 🙏');
+}
