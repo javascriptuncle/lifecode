af176f745b1651e31f96afa113b486eb36ac0263
javascriptuncle <javascriptuncle@gmail.com>
2021-01-09 20:38:29 +0530

    #8 : me while reading MDN 😏 😒 🧐 🤨 😫 😑

---
+++ #8 : me while reading MDN 😏 😒 🧐 🤨 😫 😑.js
@@ -0,0 +1,19 @@
+// me while reading MDN 😏 😒 🧐 🤨 😫 😑
+'use strict'; // read full artile
+
+mylife.read(start("😏",
+	MDNLINK.link(											// 😏 => 😒 some IMP link
+		subLinks(subLinks(subLinks(			// 😒 => 🧐  might be IMP
+			subLinks(subLinks(subLinks(		// 🧐 => 🙄 also might be IMP
+				subLinks(subLinks())				// 🤨 => 😫 where am I !!!
+				.endOfThePage("😓").followupLinks("🤯 😨") // "Realized I am in loop! 😑"
+			)))))))
+	),
+	end(("IMP 🥺") => { // never ends 🥺
+
+		// like, share and follow
+		if('❤👌 my work') {
+			console.log('🙏 Stay ❤️ tuned 🙏');
+		}
+	})
+)
