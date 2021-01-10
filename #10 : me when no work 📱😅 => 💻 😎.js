32594763a10291d7c56c81d4c7c75cd81a09fd57
javascriptuncle <javascriptuncle@gmail.com>
2021-01-10 23:52:07 +0530

    #10 : me when no work 📱😅 => 💻 😎

---
+++ #10 : me when no work 📱😅 => 💻 😎.js
@@ -0,0 +1,18 @@
+// me when no work 📱😅 => 💻 😎
+
+myfile.freeTime.do({
+	mostTimes: [ // 80% time
+		surfNet("🌊 📱"), playGames("🎮"), watchVideos("📽️"), ...otherStuffs("😅"),
+	],
+	lessTimes:[	// 20% time
+		learn("New Stuffs 🤓"), practice("💻"), post("🖼️ 📽️")
+	]
+}).soon.change(	// will change soon 😎 💪
+	super.mostTimes = [...lessTimes],
+	super.lessTimes = [...mostTimes]
+)
+
+// like, share and follow
+if ('✨ 👌 my work') {
+	console.log('🙏 Stay ❤️ tuned 🙏');
+}
