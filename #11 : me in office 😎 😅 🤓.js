37f4eabb23c77c07d4e1a7b0ca3598b7d6580add
javascriptuncle <javascriptuncle@gmail.com>
2021-01-11 22:58:58 +0530

    #11 : me in office 😎 😅 🤓

---
+++ #11 : me in office 😎 😅 🤓.js
@@ -0,0 +1,18 @@
+// me in office 😎 😅 🤓
+
+mylife.office.work.onNewWork.subscribe((newTask)=>{
+	newTask.start().code().test().deploy().reTest().update().end();
+}).meanWhile(newTask != 'me'){
+	switch (mood) {
+		case 'breakTime 😋' || 'videos 👨‍💻' || 'news & stories 😯' || 'gossips 🗣️ 😁' || '🥳':
+			break;
+
+		default:
+			'timepass';		// 😏 😪 😑
+	}
+};
+
+// like, share and follow
+if ('✨ 👌 my work') {
+	console.log('🙏 Stay ❤️ tuned 🙏');
+}
