63f31f96d974e8f783a33ea94155f0338fe4206b
javascriptuncle <javascriptuncle@gmail.com>
2021-01-12 23:56:56 +0530

    #12 : me when in sleep 🛌 💤

---
+++ #12 : me when in sleep 🛌 💤.js
@@ -0,0 +1,21 @@
+// me when in sleep 🛌 💤
+
+mylife.body.beforeSleep('📱 🥱').sleeps('🛌 😴', (me)=>{
+	// when room in cool me finding blanket
+	me.myFeeling("😑");
+	
+	// when fan in on still its hot
+	me.myFeeling("😫");
+	
+	// when mosquito bites
+	me.myFeeling("😠");
+	
+	// after trying lot to sleep again
+	me.myFeeling("🤬");
+
+}).onawake("🥴 😑 😩"); // tired and lousy mood
+
+// like, share and follow
+if ('✨ 👌 my work') {
+	console.log('🙏 Stay ❤️ tuned 🙏');
+}
