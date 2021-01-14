3c6125d71066289e637ea1446d0d235b5ed0eec4
javascriptuncle <javascriptuncle@gmail.com>
2021-01-15 00:26:19 +0530

    #14 : me early in 🌞 morning 🥱 😅 😉

---
+++ #14 : me early in 🌞 morning 🥱 😅 😉.js
@@ -0,0 +1,19 @@
+// me early in 🌞 morning 🥱 😅 😉
+
+//expectation
+me.on("⏰").stop("🥱").myMood("🙂").otherStuff("😉");
+
+//reality
+me.on("⏰")
+	.snooze("😑 => 😪")							
+	.firstTime.snooze("😑 => 🤨 => 😪")
+	.secondTime.snooze("😫 => 😓")
+	.thridTime.snooze("😠")					
+	.stop("⏰ => 🛑")							// disable all alarm 😒
+	.myMood("😅")									//  will wake up early tomorrow
+	.otherStuff("😉");
+
+// like, share and follow
+if ('😄 feel the same 😃') {
+	console.log('🙏 Stay 🤗 tuned 🙏');
+}
