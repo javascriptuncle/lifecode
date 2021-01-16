aed65f43163cb168769444a2da94314e10a3315b
javascriptuncle <javascriptuncle@gmail.com>
2021-01-16 23:42:51 +0530

    #16 : me thinking of life process 🧬 🤔 😲

---
+++ #16 : me thinking of life process 🧬 🤔 😲.js
@@ -0,0 +1,11 @@
+// me thinking of life process 🧬 🤔 😲
+
+me.prototype.dad																					// 🤔
+		dad.prototype.grandfather															// 🤔
+			grandfather.prototype.greatGrandfather							// 🤔
+				greatGrandfather.prototype.greatGreatGrandfather	// 🤔
+					greatGrandfather.prototype.[...Infinity]				// 😯
+						[...Infinity].prototype.Life									// 😲
+
+// what do you think?
+console.log('Comment && 🙏 Stay ❤️ tuned 🙏');
