826d40e601abe26bd18b6f23a882145799f8468c
javascriptuncle <javascriptuncle@gmail.com>
2021-01-16 23:42:34 +0530

    #15 : me understanding life 🧬 🤔

---
+++ #15 : me understanding life 🧬 🤔.js
@@ -0,0 +1,14 @@
+// me understanding life 🧬 🤔
+
+// we can sum-up life process in these literals*
+objects
+while...loops
+if...else...
+break;
+promise;
+callback;
+return;
+
+if ('*any process of life' == 'missed') {
+	console.log('Comment && 🙏 Stay ❤️ tuned 🙏');
+}
