89f06bfad3be8b5f127838fbaa60fdd5cc6370be
javascriptuncle <javascriptuncle@gmail.com>
2021-01-17 09:56:54 +0530

    #17 : me(mylife🧬) made up of ⚛️ 🤔

---
+++ #17 : me(mylife🧬) made up of ⚛️ 🤔.js
@@ -0,0 +1,21 @@
+// me(mylife🧬) made up of ⚛️ 🤔
+
+mylife.body: {
+	"minerals"{
+		"molecules": {
+			"atoms": {
+				"particles":{
+					"ENERGY"		// 😯
+				}
+			}
+		}
+	},
+	"water"{
+		"molecules": {
+			atoms: ...⚛️		 // 😯
+		}
+	},
+	{...LIFE } 					// 🤔
+};
+// what do you think?
+console.log('Comment && 🙏 Stay ❤️ tuned 🙏');
