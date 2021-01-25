93c5b12bade75bf6e77c603a904f052086b85866
javascriptuncle <javascriptuncle@gmail.com>
2021-01-25 22:54:14 +0530

    #25 : me working on frameworks 🔬 🤔 😉

---
+++ #25 : me working on frameworks 🔬 🤔 😉.js
@@ -0,0 +1,17 @@
+// me working on frameworks 🔬 🤔 😉
+
+switch (framework) {
+	case "Angular": 
+		// for Structured & Scalable Projects
+	case "React": 
+		// for Small Projects & New Ideas
+	case "Vue":
+		// for Small Projects & Experiments
+	case "Svelte":
+		// only for Experiments & Plugins
+	default: // Jquery
+		// works most of the time
+}
+
+// what do you think? let me know in comment
+"🙏 Stay ❤️ tuned 🙏"
