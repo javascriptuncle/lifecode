1c24efc2e88bdc1671326de71110570a657d2708
javascriptuncle <javascriptuncle@gmail.com>
2021-01-26 13:36:43 +0530

    #26 : my talk with testers 🙂 😅 🙃

---
+++ #26 : my talk with testers 🙂 😅 🙃.js
@@ -0,0 +1,18 @@
+// my talk with testers 🙂 😅 🙃
+
+// after entering inputs
+if(you.click("button")){
+	you.will.get("Expected Output")
+}
+
+// ** beneath(js) stuff **
+// onButtonclick.call.someDeadlyFuntion()"
+someDeadlyFuntion = () => {
+	var usefulData = this.getUniversalInformation()
+	var validatedData = this.parseAndValidate(usefulData);
+	var simpleResult = this.dosomeDeadlyStuff(validatedData);
+	return simpleResult;
+}
+
+// like, share and follow
+('❤👌 my work') ? 'Please 👍 😄 🤝 ' : "🙏 Stay ❤️ tuned 🙏"
